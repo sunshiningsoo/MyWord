0f119d7b9ea1d58db1d08bd5ad80e5cad6c14672
sunshiningsoo <yenkee1219@gmail.com>
2023-01-11 14:22:05 +0900

    [Enhancement] WordCard flip 액션 추가
    유저 피드백 바탕으로 WordCard Flip 액션을 추가했습니다.

--- MyWord/MyWord/View/WordList/WordCardView.swift
+++ MyWord/MyWord/View/WordList/WordCardView.swift
@@ -9,37 +9,77 @@ import SwiftUI
 
 struct WordCardView: View {
     @State var meaningShow: Bool = false
+    @State var frontAngle = Angle(degrees: 0)
+    @State var backAngle = Angle(degrees: 90)
     var word: Word
     
     var body: some View {
         ZStack {
-            Rectangle()
-                .frame(height: 100)
-                .foregroundColor(Color(.systemOrange))
-                .cornerRadius(20)
-                .shadow(radius: 3)
-                .padding()
-            HStack {
-                VStack (alignment: .leading) {
-                    Text(word.alphabet)
+            ZStack {
+                Rectangle()
+                    .frame(height: 100)
+                    .foregroundColor(Color(.systemOrange))
+                    .cornerRadius(20)
+                    .shadow(radius: 3)
+                    .padding()
+                HStack {
+                    Text(word.meaning)
                         .font(.title)
                         .fontWeight(.bold)
-                    if !word.example.isEmpty {
-                        Text(word.example)
-                            .font(.subheadline)
-                            .fontWeight(.regular)
-                    }
-                }.accentColor(Color(.systemBackground))
-                Spacer()
-                Text(meaningShow ? word.meaning : "뜻 보기")
-                    .font(.title3)
-                    .foregroundColor(Color(.systemBackground))
-                    .onTapGesture {
-                        meaningShow.toggle()
-                    }
+                        .foregroundColor(Color(.systemBackground))
+                        .onTapGesture {
+                            meaningShow.toggle()
+                            withAnimation(.linear(duration: 0.2)) {
+                                backAngle += Angle(degrees: meaningShow ? -90 : 90)
+                            }
+                            withAnimation(.linear(duration: 0.2).delay(0.2)) {
+                                frontAngle += Angle(degrees: meaningShow ? -90 : 90)
+                                
+                            }
+                        }
+                }
+                .padding()
+                .frame(width: UIScreen.screenWidth - 40, height: 100)
             }
-            .padding()
-            .frame(width: UIScreen.screenWidth - 40, height: 100)
+            .rotation3DEffect(backAngle, axis: (x: 0, y: 1, z: 0))
+            
+            ZStack {
+                Rectangle()
+                    .frame(height: 100)
+                    .foregroundColor(Color(.systemOrange))
+                    .cornerRadius(20)
+                    .shadow(radius: 3)
+                    .padding()
+                HStack {
+                    VStack (alignment: .leading) {
+                        Text(word.alphabet)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        if !word.example.isEmpty {
+                            Text(word.example)
+                                .font(.subheadline)
+                                .fontWeight(.regular)
+                        }
+                    }.accentColor(Color(.systemBackground))
+                    Spacer()
+                    Text("뜻 보기")
+                        .rotation3DEffect(frontAngle, axis: (x: 1, y: 0, z: 0))
+                        .font(.title3)
+                        .foregroundColor(Color(.systemBackground))
+                        .onTapGesture {
+                            meaningShow.toggle()
+                            withAnimation(.linear(duration: 0.2)) {
+                                frontAngle += Angle(degrees: meaningShow ? -90 : 90)
+                            }
+                            withAnimation(.linear(duration: 0.2).delay(0.2)) {
+                                backAngle += Angle(degrees: meaningShow ? -90 : 90)
+                            }
+                        }
+                }
+                .padding()
+                .frame(width: UIScreen.screenWidth - 40, height: 100)
+            }
+            .rotation3DEffect(frontAngle, axis: (x: 0, y: 1, z: 0))
         }
     }
 }
